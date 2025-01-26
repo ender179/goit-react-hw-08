02a574190a8a4266e742add50c1e2deabf759bf3
ender179 <kirilukvlad91@gmail.com>
2025-01-26 19:23:49 +0200

    git push

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,35 @@
-import React from 'react';  
-import { useDispatch, useSelector } from 'react-redux';  
-import { Routes, Route } from 'react-router-dom';    
-import HomePage from './pages/HomePage';  
-import ContactsPage from './pages/ContactsPage';  
-import LoginPage from './pages/LoginPage';  
-import RegistrationPage from './pages/RegistrationPage';  
-import { checkAuthStatus } from './redux/auth/operations'; 
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
-const App = () => {  
-  const dispatch = useDispatch();  
-  const isRefreshing = useSelector(state => state.auth.isRefreshing);  
+function App() {
+  const [count, setCount] = useState(0)
 
-  React.useEffect(() => {  
-    dispatch(checkAuthStatus());  
-  }, [dispatch]);  
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
 
-  return (  
-    <>  
-      {isRefreshing ? (  
-        <Loading />  
-      ) : (  
-        <Routes>  
-          <Route path="/" element={<HomePage />} />  
-          <Route path="/contacts" element={<ContactsPage />} />  
-          <Route path="/login" element={<LoginPage />} />  
-          <Route path="/registration" element={<RegistrationPage />} />  
-        </Routes>  
-      )}  
-    </>  
-  );  
-};  
-
-export default App;
+export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,10 @@
-import React, { StrictMode } from 'react';  
-import { createRoot } from 'react-dom/client';  
-import { Provider } from 'react-redux';  
-import { BrowserRouter } from 'react-router-dom';  
-import store from './redux/store'; 
-import App from './App';  
-import './index.css'; 
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
 
-const root = createRoot(document.getElementById('root'));  
-
-root.render(  
-    <StrictMode>  
-        <Provider store={store}> 
-            <BrowserRouter>  
-                <App />
-            </BrowserRouter>  
-        </Provider>  
-    </StrictMode>  
-);
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
