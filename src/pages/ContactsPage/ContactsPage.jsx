21eeac1918487db01f7de46c770db0824edcb094
ender179 <kirilukvlad91@gmail.com>
2025-01-28 15:45:09 +0200

    git push

--- src/pages/ContactsPage/ContactsPage.jsx
+++ src/pages/ContactsPage/ContactsPage.jsx
@@ -1,32 +1,37 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { fetchContacts } from '../../redux/contacts/operations';
-import { selectContacts, selectIsLoading, selectError } from '../../redux/contacts/selectors';
-import ContactForm from '../../components/ContactForm/ContactForm';
-import ContactList from '../../components/ContactList/ContactList';
-import Filter from '../../components/Filter/Filter';
-import styles from './ContactsPage.module.css';
+import React, { useEffect } from 'react';  
+import { useDispatch, useSelector } from 'react-redux';  
+import { fetchContacts } from '../../redux/contacts/operations';  
+import { selectContacts, selectIsLoading, selectError } from '../../redux/contacts/selectors';  
+import AppBar from '../../components/AppBar/AppBar'; 
+import AuthNav from '../../components/AuthNav/AuthNav'; 
+import Navigation from '../../components/Navigation/Navigation';  
+import UserMenu from '../../components/UserMenu/UserMenu'; 
+import styles from './ContactsPage.module.css'; 
 
-const ContactsPage = () => {
-  const dispatch = useDispatch();
-  const contacts = useSelector(selectContacts);
-  const isLoading = useSelector(selectIsLoading);
-  const error = useSelector(selectError);
+const ContactsPage = () => {  
+  const dispatch = useDispatch();  
+  const contacts = useSelector(selectContacts);  
+  const isLoading = useSelector(selectIsLoading);  
+  const error = useSelector(selectError);  
 
-  useEffect(() => {
-    dispatch(fetchContacts());
-  }, [dispatch]);
+  useEffect(() => {  
+    dispatch(fetchContacts());  
+  }, [dispatch]);  
 
-  return (
-    <div className={styles.container}>
-      <h1>Your Contacts</h1>
-      <ContactForm />
-      <Filter />
-      {isLoading && <p>Loading...</p>}
-      {error && <p>{error}</p>}
-      <ContactList contacts={contacts} />
-    </div>
-  );
-};
+  return (  
+    <div className={styles.container}>  
+      <AppBar /> {/* Добавление AppBar */}  
+      <AuthNav /> {/* Добавление AuthNav */}  
+      <Navigation /> {/* Добавление Navigation */}  
+      <UserMenu /> {/* Добавление UserMenu */}  
+      <h1>Your Contacts</h1>  
+      <ContactForm />  
+      <Filter />  
+      {isLoading && <p>Loading...</p>}  
+      {error && <p>{error}</p>}  
+      <ContactList contacts={contacts} />  
+    </div>  
+  );  
+};  
 
 export default ContactsPage;
