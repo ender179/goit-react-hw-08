c0e7d51f09055ec5cc1d3db858c24c0eaff5b536
ender179 <kirilukvlad91@gmail.com>
2025-01-29 12:25:04 +0200

    git push

--- src/redux/filters/Slice.js
+++ src/redux/filters/Slice.js
@@ -1,12 +1,12 @@
 import { createSlice } from '@reduxjs/toolkit';  
 
 const filtersSlice = createSlice({  
-  name: 'filters',  
-  initialState: '',  
-  reducers: {  
-    setFilter: (state, action) => action.payload,  
-  },  
+    name: 'filters',  
+    initialState: '',  
+    reducers: {  
+        setFilter: (state, action) => action.payload,  
+    },  
 });  
 
 export const { setFilter } = filtersSlice.actions;  
-export default filtersSlice.reducer; 
+export default filtersSlice.reducer;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,30 +1,24 @@
 import { configureStore } from '@reduxjs/toolkit';  
 import { persistStore, persistReducer } from 'redux-persist';  
-import storage from 'redux-persist/lib/storage'; 
+import storage from 'redux-persist/lib/storage';  
 import authReducer from './auth/slice';  
 import contactsReducer from './contacts/slice';  
-import filtersReducer from './filters/slice'; 
+import filtersReducer from './filters/slice';  
 
 const authPersistConfig = {  
-  key: 'auth',  
-  storage,  
-  whitelist: ['token'], 
+    key: 'token',  
+    storage,  
+    whitelist: ['token'],  
 };  
 
 const store = configureStore({  
-  reducer: {  
-    auth: persistReducer(authPersistConfig, authReducer),  
-    contacts: contactsReducer,  
-    filters: filtersReducer, 
-  },  
-  middleware: (getDefaultMiddleware) =>  
-    getDefaultMiddleware({  
-      serializableCheck: {  
-        ignoredActions: ['persist/PERSIST', 'persist/REHYDRATE', 'persist/PAUSE', 'persist/PURGE', 'persist/REGISTER'],  
-      },  
-    }),  
+    reducer: {  
+        auth: persistReducer(authPersistConfig, authReducer),  
+        contacts: contactsReducer,  
+        filters: filtersReducer,  
+    },  
 });  
 
 const persistor = persistStore(store);  
 
-export { store, persistor }; 
+export { store, persistor };
