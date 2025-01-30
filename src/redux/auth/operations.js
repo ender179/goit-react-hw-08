86a11d089d5bea5f82558ccf00b66cf5f39d53cc
ender179 <kirilukvlad91@gmail.com>
2025-01-30 18:11:35 +0200

    git push

--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -28,23 +28,47 @@ export const login = createAsyncThunk(
 export const logOut = createAsyncThunk(
   'auth/logOut',
   async (arg, thunkAPI) => {
+    try {
+      await axios.post('/api/auth/logout');
+      return {};
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
   }
 );
 
 export const fetchContacts = createAsyncThunk(
   'contacts/fetchContacts',
   async (arg, thunkAPI) => {
+    try {
+      const response = await axios.get('/api/contacts');
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
   }
 );
 
 export const addContact = createAsyncThunk(
   'contacts/addContact',
-  async (arg, thunkAPI) => {
+  async (newContact, thunkAPI) => {
+    try {
+      const response = await axios.post('/api/contacts', newContact);
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
   }
 );
 
 export const deleteContact = createAsyncThunk(
   'contacts/deleteContact',
-  async (arg, thunkAPI) => {
+  async (contactId, thunkAPI) => {
+    try {
+      await axios.delete(`/api/contacts/${contactId}`);
+      return contactId;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
   }
 );
