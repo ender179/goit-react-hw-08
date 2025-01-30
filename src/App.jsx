3eb6a21621e23c036c6afb611fe7cd985f8569ee
ender179 <kirilukvlad91@gmail.com>
2025-01-30 15:47:28 +0200

    git push

--- src/App.jsx
+++ src/App.jsx
@@ -5,31 +5,31 @@ import HomePage from './pages/HomePage/HomePage';
 import RegistrationPage from './pages/RegistrationPage/RegistrationPage';  
 import LoginPage from './pages/LoginPage/LoginPage';  
 import ContactsPage from './pages/ContactsPage/ContactsPage';  
-import Layout from './Layout'; 
-import { checkAuthStatus } from './redux/auth/authActions'; 
+import Layout from './Layout'; // Убедитесь, что путь правильный.  
+import checkAuthStatus from './redux/auth/authActions';  
 
 const App = () => {  
-  const dispatch = useDispatch();  
-  const isRefreshing = useSelector((state) => state.auth.isRefreshing);
+    const dispatch = useDispatch();  
+    const isRefreshing = useSelector((state) => state.auth.isRefreshing);  
 
-  useEffect(() => {  
-    dispatch(checkAuthStatus());  
-  }, [dispatch]);  
+    useEffect(() => {  
+        dispatch(checkAuthStatus());  
+    }, [dispatch]);  
 
-  return (  
-    <Layout>  
-      {isRefreshing ? (  
-        <p>Loading...</p> 
-      ) : (  
-        <Routes>  
-          <Route path="/" element={<HomePage />} />  
-          <Route path="/register" element={<RegistrationPage />} />  
-          <Route path="/login" element={<LoginPage />} />  
-          <Route path="/contacts" element={<ContactsPage />} />  
-        </Routes>  
-      )}  
-    </Layout>  
-  );  
+    return (  
+        <Layout>  
+            {isRefreshing ? (  
+                <p>Loading...</p>  
+            ) : (  
+                <Routes>  
+                    <Route path="/" element={<HomePage />} />  
+                    <Route path="/register" element={<RegistrationPage />} />  
+                    <Route path="/login" element={<LoginPage />} />  
+                    <Route path="/contacts" element={<ContactsPage />} />  
+                </Routes>  
+            )}  
+        </Layout>  
+    );  
 };  
 
 export default App;
--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,15 +1,17 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
-import AppBar from '../AppBar/AppBar';
-import styles from './Layout.module.css';
+import React from 'react';  
+import { Outlet } from 'react-router-dom';  
+import AppBar from '../AppBar/AppBar';  
+import styles from './Layout.module.css';  
 
-const Layout = () => (
-  <div className={styles.layout}>
-    <AppBar />
-    <main>
-      <Outlet />
-    </main>
-  </div>
-);
+const Layout = () => {  
+    return (  
+        <div className={styles.layout}>  
+            <AppBar />  
+            <main>  
+                <Outlet />  
+            </main>  
+        </div>  
+    );  
+};  
 
 export default Layout;
