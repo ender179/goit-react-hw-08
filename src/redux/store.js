e723511a8b3923a6b5dbbb87a6bc6fe23a82bb1f
ender179 <kirilukvlad91@gmail.com>
2025-01-31 13:25:52 +0200

    git push

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { combineReducers } from 'redux';
 import contactsReducer from './contacts/slice';
-import filtersReducer from './filters/slice';
+import filtersReducer from './filters/Slice';
 import authReducer from './auth/slice';
 
 const rootReducer = combineReducers({
