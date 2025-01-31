83d44d4a72013a378822fd8aeccceb6f87843d3c
ender179 <kirilukvlad91@gmail.com>
2025-01-31 16:41:47 +0200

    git push

--- src/redux/auth/slice.js
+++ src/redux/auth/slice.js
@@ -1,38 +1,30 @@
-import { createSlice } from '@reduxjs/toolkit';  
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';  
+import axios from 'axios';  
+
+export const register = createAsyncThunk('auth/register', async (userData) => {  
+    const response = await axios.post('/users/register', userData);  
+    return response.data;  
+});  
 
 const initialState = {  
-  user: null,  
-  token: localStorage.getItem('token') || null,  
-  isLoggedIn: false,  
-  isRefreshing: false,  
+    user: null,  
+    token: localStorage.getItem('token') || null,  
+    isLoggedIn: false,  
+    isRefreshing: false,  
 };  
 
 const authSlice = createSlice({  
-  name: 'auth',  
-  initialState,  
-  reducers: {},  
-  extraReducers: (builder) => {  
-    builder  
-      .addCase(register.fulfilled, (state, action) => {  
-        state.user = action.payload.user;  
-        state.token = action.payload.token;  
-        state.isLoggedIn = true;  
-      })  
-      .addCase(login.fulfilled, (state, action) => {  
-        state.user = action.payload.user;  
-        state.token = action.payload.token;  
-        state.isLoggedIn = true;  
-      })  
-      .addCase(logOut.fulfilled, (state) => {  
-        state.user = null;  
-        state.token = null;  
-        state.isLoggedIn = false;  
-      })  
-      .addCase(refreshUser.fulfilled, (state, action) => {  
-        state.user = action.payload;  
-        state.isLoggedIn = true;  
-      });  
-  },  
+    name: 'auth',  
+    initialState,  
+    reducers: {},  
+    extraReducers: (builder) => {  
+        builder  
+            .addCase(register.fulfilled, (state, action) => {  
+                state.user = action.payload.user;  
+                state.token = action.payload.token;  
+                state.isLoggedIn = true;  
+            })  
+    },  
 });  
 
 export default authSlice.reducer;
