67f30793563f003ff76cdd3bee2270da661b5b7c
ender179 <kirilukvlad91@gmail.com>
2025-01-31 19:29:07 +0200

    git push

--- src/redux/auth/slice.js
+++ src/redux/auth/slice.js
@@ -1,8 +1,6 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';  
 
-// Пример операции регистрации  
 export const register = createAsyncThunk('auth/register', async (userData) => {  
-    // Замените URL и метод на свои  
     const response = await fetch('/api/register', {  
         method: 'POST',  
         body: JSON.stringify(userData),  
