71b80bd0dde5d52afefbfa2b0ed738c8e2cc72bf
ender179 <kirilukvlad91@gmail.com>
2025-01-31 15:47:51 +0200

    git push

--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -1,72 +1,21 @@
-import axios from 'axios';
-import { createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';  
+import { createAsyncThunk } from '@reduxjs/toolkit';  
 
-axios.defaults.baseURL = 'https://connections-api.goit.global/';
+axios.defaults.baseURL = 'https://connections-api.goIt's.global/'; // Убедитесь, что URL корректен  
 
-const setAuthHeader = token => {
-  axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
-};
+const setAuthHeader = (token) => {  
+  axios.defaults.headers.common.Authorization = `Bearer ${token}`;  
+};  
 
-const clearAuthHeader = () => {
-  axios.defaults.headers.common['Authorization'] = '';
-};
+const clearAuthHeader = () => {  
+  axios.defaults.headers.common.Authorization = '';  
+};  
 
-export const register = createAsyncThunk(
-  'auth/register',
-  async (userData, thunkAPI) => {
-    try {
-      const response = await axios.post('/users/signup', userData);
-      const { token } = response.data;
-      localStorage.setItem('token', token);
-      setAuthHeader(token);
-      return response.data;
-    } catch (error) {
-      return thunkAPI.rejectWithValue(error.message);
-    }
-  }
-);
-
-export const login = createAsyncThunk(
-  'auth/login',
-  async (userData, thunkAPI) => {
-    try {
-      const response = await axios.post('/users/login', userData);
-      const { token } = response.data;
-      localStorage.setItem('token', token);
-      setAuthHeader(token);
-      return response.data;
-    } catch (error) {
-      return thunkAPI.rejectWithValue(error.message);
-    }
-  }
-);
-
-export const logOut = createAsyncThunk('auth/logOut', async (_, thunkAPI) => {
-  try {
-    await axios.post('/users/logOut');
-    localStorage.removeItem('token');
-    clearAuthHeader();
-  } catch (error) {
-    return thunkAPI.rejectWithValue(error.message);
-  }
+export const logout = createAsyncThunk('auth/logout', async (_, thunkAPI) => {  
+  try {  
+    await axios.post('/users/logout');  
+    clearAuthHeader();  
+  } catch (error) {  
+    return thunkAPI.rejectWithValue(error.message);  
+  }  
 });
-
-export const refreshUser = createAsyncThunk(
-  'auth/refresh',
-  async (_, thunkAPI) => {
-    const token = localStorage.getItem('token');
-
-    if (!token) {
-      return thunkAPI.rejectWithValue('No token found');
-    }
-
-    setAuthHeader(token);
-
-    try {
-      const response = await axios.get('/users/current');
-      return response.data;
-    } catch (error) {
-      return thunkAPI.rejectWithValue(error.message);
-    }
-  }
-);
