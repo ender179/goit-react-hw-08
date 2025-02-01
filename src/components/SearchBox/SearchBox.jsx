fdf0e657d4a38cedc2e629727040cfbe6b498b8c
ender179 <kirilukvlad91@gmail.com>
2025-02-01 13:28:52 +0200

    git push

--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -1,32 +1,33 @@
-import { useDispatch, useSelector } from 'react-redux';
-import { changeFilter } from '../../redux/filters/Slice';
-import { selectFilterValue } from '../../redux/filters/selectors';
-import styles from './SearchBox.module.css';
+import { useDispatch, useSelector } from 'react-redux';  
+import { changeFilter } from '../../redux/filters/Slice';  
+import { selectFilterValueByName } from '../../redux/filters/selectors'; // Используем правильный селектор  
+import styles from './SearchBox.module.css';  
 
-const SearchBox = () => {
-  const dispatch = useDispatch();
-  const filter = useSelector(selectFilterValue);
+const SearchBox = () => {  
+  const dispatch = useDispatch();  
+  // Замените 'name' на реальное имя фильтра, который вам нужен  
+  const filter = useSelector(state => selectFilterValueByName(state, 'name')); // Используем селектор и передаём имя фильтра  
 
-  const handleChange = e => {
-    dispatch(changeFilter(e.target.value.toLowerCase()));
-  };
+  const handleChange = e => {  
+    dispatch(changeFilter(e.target.value.toLowerCase()));  
+  };  
 
-  return (
-    <div className={styles.searchBox}>
-      <label htmlFor="search" className={styles.label}>
-        Find contacts by name
-      </label>
-      <input
-        id="search"
-        name="search"
-        type="text"
-        value={filter}
-        onChange={handleChange}
-        className={styles.input}
-        autoComplete="off"
-      />
-    </div>
-  );
-};
+  return (  
+    <div className={styles.searchBox}>  
+      <label htmlFor="search" className={styles.label}>  
+        Find contacts by name  
+      </label>  
+      <input  
+        id="search"  
+        name="search"  
+        type="text"  
+        value={filter}  
+        onChange={handleChange}  
+        className={styles.input}  
+        autoComplete="off"  
+      />  
+    </div>  
+  );  
+};  
 
 export default SearchBox;
