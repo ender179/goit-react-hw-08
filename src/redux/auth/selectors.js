695b8e6444c312e9338f0373305aad0259ad16fd
ender179 <kirilukvlad91@gmail.com>
2025-02-01 13:55:37 +0200

    git push

--- src/redux/auth/selectors.js
+++ src/redux/auth/selectors.js
@@ -1,3 +1,5 @@
-export const selectIsRefreshing = (state) => state.auth.isRefreshing;
+export const selectIsRefreshing = (state) => state.auth.isRefreshing;  
 
-export const selectUser = (state) => state.auth.user; 
+export const selectUser = (state) => state.auth.user;  
+
+export const selectIsLoggedIn = (state) => state.auth.isLoggedIn; // Новый селектор
