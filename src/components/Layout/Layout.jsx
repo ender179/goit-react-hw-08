cff58d6b91efb176480e87b09b6455ea59a9930e
ender179 <kirilukvlad91@gmail.com>
2025-02-03 16:03:18 +0200

    git push

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,13 +1,15 @@
 import React from 'react';  
+import { Outlet } from 'react-router-dom'; 
 import AppBar from '../AppBar/AppBar';  
 
 const Layout = ({ children }) => {  
     return (  
         <div>  
-            <AppBar />  
-            {children}  
+            <AppBar />    
+            <Outlet />   
+            {children}     
         </div>  
     );  
 };  
 
-export default Layout;
+export default Layout; 
